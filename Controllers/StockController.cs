b74d99dc58b040cd221fbc1a983583a130a169d9
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-09-12 11:46:45 +0200

    feat : Update Stock

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -13,7 +13,6 @@ namespace api.Controllers
     [ApiController]
     public class StockController : ControllerBase
     {
-
         #region attributes
 
         private readonly ApplicationDBContext _context;
@@ -41,6 +40,7 @@ namespace api.Controllers
             {
                 return NotFound();
             }
+
             return Ok(stock.ToStockDto());
         }
 
@@ -51,6 +51,28 @@ namespace api.Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges(); // méthode très importante car sans cette ligne, l'objet n'est pas sauvegardé dans la base de données/
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto stockDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = stockDto.Symbol;
+            stockModel.CompanyName = stockDto.CompanyName;
+            stockModel.Purchase = stockDto.Purchase;
+            stockModel.LastDividend = stockDto.LastDividend;
+            stockModel.Industry = stockDto.Industry;
+            stockModel.MarketCap = stockDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
